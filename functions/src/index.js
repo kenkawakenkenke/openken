1cdaf66697995f6d8101ba9ba731717b6869e02a
Ken Kawamoto <ken@kawamoto.co.uk>
2021-03-09 05:35:41 +0900

    Initial test functions

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,9 +1,25 @@
 const functions = require("firebase-functions");
+const firebase = require('firebase-admin');
+firebase.initializeApp(functions.config().firebase);
+const firestore = firebase.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// https://asia-northeast1-open-ken.cloudfunctions.net/submitSensorData
+exports.submitSensorData = functions
+    .region('asia-northeast1')
+    .https.onCall((data, context) => {
+        const uid = context.auth.uid;
+        const sensorData = data.sensorData;
+        console.log(uid, sensorData);
+        return {
+            status: "ok",
+            message: "hello world! " + uid + " " + context.auth.token.name,
+        };
+    });
+
+// https://asia-northeast1-open-ken.cloudfunctions.net/hello
+exports.hello = functions.region("asia-northeast1")
+    .https
+    .onRequest(async (req, res) => {
+        res.status(200).send("Hello cloud functions!");
+        return;
+    });
