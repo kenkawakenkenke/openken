17b347eb4b58efdb0e74e3ae7e16446d28414912
Ken Kawamoto <ken@kawamoto.co.uk>
2021-03-09 11:32:26 +0900

    Update some function comments.

--- functions/src/index.js
+++ functions/src/index.js
@@ -4,6 +4,14 @@ firebase.initializeApp(functions.config().firebase)
 const { onCallGenerateAccessToken } = require("./access_token_gen.js");
 const { onSubmitSensorData } = require("./data_receiver.js");
 
+// https://asia-northeast1-open-ken.cloudfunctions.net/hello
+exports.hello = functions.region("asia-northeast1")
+    .https
+    .onRequest(async (req, res) => {
+        res.status(200).send("Hello cloud functions!");
+        return;
+    });
+
 // Handler for receiving raw data from Fitbit device.
 // Expects parameters:
 // token: access token
@@ -14,14 +22,8 @@ exports.submitSensorData = functions
     .https
     .onRequest(onSubmitSensorData);
 
-// https://asia-northeast1-open-ken.cloudfunctions.net/hello
-exports.hello = functions.region("asia-northeast1")
-    .https
-    .onRequest(async (req, res) => {
-        res.status(200).send("Hello cloud functions!");
-        return;
-    });
-
+// Handler for regenerating access tokens for the logged in user.
+// Called from https://open-ken.web.app/access_token.
 exports.generateAccessToken =
     functions
         .region("asia-northeast1")
