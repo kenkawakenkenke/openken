5a5d08d29aaf5cbafe84b3e5c0af7066714437c0
Ken Kawamoto <ken@kawamoto.co.uk>
2021-03-13 17:36:35 +0900

    Add more comments to fitbit app.

--- fitbit/app/index.js
+++ fitbit/app/index.js
@@ -9,9 +9,16 @@ import UI from "./ui/index.js";
 import SensorDataCollector from "./sensor_collector.js";
 import DataSender from "./data_sender.js";
 
+/**
+ * dataCollector collects sensor data, calls callback with data every 30 seconds.
+ * callback calls dataSender, which calls companion requester to send data to the server.
+ * requester calls back with the server response,
+ * and dataSender calls UI to update the status label.
+ */
 const ui = new UI();
 const dataSender = new DataSender(ui);
-const dataCollector = new SensorDataCollector(30, data => dataSender.handleData(data));
+const dataCollector = new SensorDataCollector(30,
+    data => dataSender.handleData(data));
 
 // Ensure we stay alive.
 me.appTimeoutEnabled = false;
@@ -28,7 +35,8 @@ messaging.peerSocket.addEventListener("error", (err) => {
     ui.showStatusText("Connection error: " + err.message, false);
 });
 
-// Setup clock
+// Setup clock. Every Tick, we update the clock face and 
+// status/metric labels.
 clock.granularity = "seconds";
 clock.addEventListener("tick", (evt) => {
     const now = new Date();
