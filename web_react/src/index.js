07cadebd0da3aa32540e66f89b0225b22fd666fa
Ken Kawamoto <ken@kawamoto.co.uk>
2021-03-14 17:54:22 +0900

    Enable firebase analytics

--- web_react/src/index.js
+++ web_react/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import "./backend/firebase_config.js";
+import firebase from "./backend/firebase_config.js";
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
